9f739162f785f815f77a62b6b9ede837e4cac89f
matthewperiut <matthewperiut@gmail.com>
2024-06-20 23:45:16 -0400

    1.20.4

---
+++ neoforge/src/main/java/com/matthewperiut/fence_on_slab/neoforge/FenceOnSlabNeoForge.java
@@ -0,0 +1,11 @@
+package com.matthewperiut.fence_on_slab.neoforge;
+
+import com.matthewperiut.fence_on_slab.FenceOnSlab;
+import net.neoforged.fml.common.Mod;
+
+@Mod(FenceOnSlab.MOD_ID)
+public class FenceOnSlabNeoForge {
+    public FenceOnSlabNeoForge() {
+        FenceOnSlab.init();
+    }
+}
